122ce95818457ef8cf0c98b670cd01f5d381cf05
yigitsargindev <yigitsargin@gmail.com>
2024-08-03 19:00:56 +0300

    fix: eslint prettier configuration

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            '@/components': './components',
+            '@/constants': './constants',
+            '@/screens': './screens',
+            '@/utils': './utils',
+          },
+        },
+      ],
+    ],
   };
 };
